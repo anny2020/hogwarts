75b514dcacaf1b77b1f1a209169a425dfb29b6e2
annyzxh <18537810265@163.com>
2020-06-17 16:51:27 +0800

    pytest实战二作业：修改datas

--- homework/conftest.py
+++ homework/conftest.py
@@ -7,24 +7,24 @@ def start():
     yield
     print("-----------计算结束-------------")
 
-def pytest_addoption(parser, pluginmanager):
+def pytest_addoption(parser):
     mygroup = parser.getgroup("anny")
     mygroup.addoption("--env",default='test',dest='env',help='set your run env')
 
 @pytest.fixture(scope="session")
 def cmdoption(request):
-    myenv =request.config.getoption("--env",default='test')
+    myenv =request.config.getoption("--env",default="test")
     if myenv == 'test':
         print("get test data")
         with open("datas/test/test.yml") as f:
-            print(yaml.safe_load(f))
+            datas = yaml.safe_load(f)
     elif myenv == "dev":
         print("get dev data")
         with open("datas/dev/dev.yml") as f:
-            print(yaml.safe_load(f))
+            datas = yaml.safe_load(f)
     elif myenv == "st":
         print("get st data")
         with open("datas/st/st.yml") as f:
-            print(yaml.safe_load(f))
-    return myenv
+            datas = yaml.safe_load(f)
+    return datas
 
