a0e2e798d75ac5bb1317a737d4eafc9521e6a8ca
annyzxh <18537810265@163.com>
2020-07-08 12:01:38 +0800

    appium企业微信实战二：创建datas目录，将data.yml放入其中

--- homework/appium_wework/wework_po/testcases/test_contact.py
+++ homework/appium_wework/wework_po/testcases/test_contact.py
@@ -15,7 +15,7 @@ class TestWeWork:
         self.app.close()
 
     @pytest.mark.parametrize(['name','sex','phone','mailbox','address','identity'],
-                             yaml.safe_load(open("data.yaml",encoding='utf-8'))['add'])
+                             yaml.safe_load(open("../datas/data.yaml", encoding='utf-8'))['add'])
     def test_add_contact(self,name,sex,phone,mailbox,address,identity):
         toast = self.main.goto_addresslist().goto_memberinvite()\
                         .goto_contactadd().edit_username(name).edit_sex(sex).edit_phone(phone)\
@@ -24,7 +24,7 @@ class TestWeWork:
         assert toast == '添加成功'
         self.main.back()
 
-    @pytest.mark.parametrize(['name1','name2'],yaml.safe_load(open("data.yaml",encoding='utf-8'))['del'])
+    @pytest.mark.parametrize(['name1','name2'], yaml.safe_load(open("../datas/data.yaml", encoding='utf-8'))['del'])
     def test_del_contact(self,name1,name2):
         result = self.main.goto_addresslist().goto_contactdetail_briefinfo(name1)\
             .goto_contactdetail_setting().goto_editmember().del_member().del_confirm().member_disappear(name2)
